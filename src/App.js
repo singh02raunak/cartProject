5274e7384314831d1de7844ff9390e00c3f171da
Raunak Singh <singh02raunak@gmail.com>
2022-02-04 19:27:14 +0530

    first

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Login from "./component/Authentication/Login";
 import SignUp from "./component/Authentication/SignUp";
-// import Header from "./component/Layout/Header";
 import Home from "./component/Layout/Home";
 import Landing from "./component/Product/Landing";
 
 function App() {
   return (
     <div className="App">
-      {/* <Header /> */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Landing />} />
